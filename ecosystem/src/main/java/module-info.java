fc87f6b01d9461e0ca3a5de57340a955fab8b5e5
rfechner <richard.fechner.nr@gmail.com>
2021-11-12 20:53:49 +0100

    added some cells in the jupyter notebook, nothing too fancy

--- ecosystem/src/main/java/module-info.java
+++ ecosystem/src/main/java/module-info.java
@@ -3,8 +3,11 @@ module com.example.ecosystem {
     requires javafx.fxml;
 
     exports de.uniko.ecosystem.control;
+    exports de.uniko.ecosystem.model.trees;
+    exports de.uniko.ecosystem;
+
 
     opens de.uniko.ecosystem.control to javafx.fxml;
-    exports de.uniko.ecosystem;
+
     opens de.uniko.ecosystem to javafx.fxml;
 }
